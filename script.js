ceda1f8f41c7d6c49d9b8644ba76b9d29e415820
meriquiroga <meriquiroga@gmail.com>
2024-01-14 11:52:26 -0300

    To spanish

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function createCard(users) {
         <img class="img" src="${user.picture.large}" alt="" />
         <h4 class="name">${user.name.first} ${user.name.last}</h4>
         <div class="age">
-            <p>Age: ${user.registered.age}</p>
+            <p>Edad: ${user.registered.age}</p>
         </div>
         <div class="gender">
-            <p class="genderText">${user.gender.toUpperCase()}</p>
+            <p class="genderText">${user.gender === "female" ? "MUJER" : "HOMBRE"}</p>
         </div>
         <div class="info">
             <i class="fas fa-xs fa-phone fa-rotate-90"></i>
@@ -28,7 +28,7 @@ function createCard(users) {
         </div>
       </div>
       <button class="button">
-         <a class="button-text" href="https://wa.me/${user.phone}" target="_blank" rel="noreferrer">Connect</a>
+         <a class="button-text" href="https://wa.me/${user.phone}" target="_blank" rel="noreferrer">Conectar</a>
       </button>
    </div>
     `;
