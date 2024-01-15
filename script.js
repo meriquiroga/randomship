3f3c9a9e204ef5b8be585b5d845e203571592c20
meriquiroga <meriquiroga@gmail.com>
2024-01-15 19:23:55 -0300

    Buscador

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
-fetch("https://randomuser.me/api/?results=12")
-  .then((response) => response.json())
-  .then((data) => {
-    createCard(data.results);
-  });
+const list = document.getElementById("list");
+const search = document.getElementById("search");
 
-function createCard(users) {
-  let container = document.querySelector(".container");
+const listUsers = [];
 
-  users.forEach((user) => {
-    container.innerHTML += `
+getUsers();
+
+search.addEventListener("input", (e) => filterUsers(e.target.value));
+
+async function getUsers() {
+  const response = await fetch("https://randomuser.me/api?results=40");
+
+  const { results } = await response.json();
+
+  list.innerHTML = "";
+
+  results.forEach((user) => {
+    const li = document.createElement("li");
+
+    listUsers.push(li);
+
+    li.innerHTML = `
     <div class="card">
-      <div class="firstBlock">
-        <img class="img" src="${user.picture.large}" alt="" />
-        <h4 class="name">${user.name.first} ${user.name.last}</h4>
-        <div class="age">
-            <p>Edad: ${user.registered.age}</p>
-        </div>
-        <div class="gender">
-            <p class="genderText">${user.gender === "female" ? "MUJER" : "HOMBRE"}</p>
-        </div>
-        <div class="info">
-            <i class="fas fa-xs fa-phone fa-rotate-90"></i>
-            <p>${user.phone}</p>
-        </div>
-        <div class="info">
-            <p>${user.location.city} | ${user.location.country}</p>
-        </div>
-      </div>
-      <button class="button">
-         <a class="button-text" href="https://wa.me/${user.phone}" target="_blank" rel="noreferrer">Conectar</a>
-      </button>
-   </div>
-    `;
+          <div class="firstBlock">
+            <img class="img" src="${user.picture.large}" alt="" />
+            <h4 class="name">${user.name.first}</h4>
+            <h4 class="name">${user.name.last}</h4>
+
+            <div class="age">
+                <p>Edad: ${user.registered.age}</p>
+            </div>
+            <div class="gender">
+                <p class="genderText">${
+                  user.gender === "female" ? "MUJER" : "HOMBRE"
+                }</p>
+            </div>
+            <div class="info">
+                <i class="fas fa-xs fa-phone fa-rotate-90"></i>
+                <p>${user.phone}</p>
+            </div>
+            <div class="info">
+                <p>${user.location.city} | ${user.location.country}</p>
+            </div>
+          </div>
+          <button class="button">
+             <a class="buttonText" href="https://wa.me/${
+               user.phone
+             }" target="_blank" rel="noreferrer">Conectar</a>
+          </button>
+       </div>
+      `;
+    list.appendChild(li);
+  });
+}
+
+function filterUsers(searchValue) {
+  listUsers.forEach((user) => {
+    if (
+      user.innerText
+        .split("\n", 2)[0]
+        .toLowerCase()
+        .startsWith(searchValue.toLowerCase()) ||
+      user.innerText
+        .split("\n", 2)[1]
+        .toLowerCase()
+        .startsWith(searchValue.toLowerCase())
+    ) {
+      user.classList.remove("hide");
+    } else {
+      user.classList.add("hide");
+    }
   });
 }
